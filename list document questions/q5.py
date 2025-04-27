a7b8abbf8fe836d6b2606f5ec083e1789ae360b0
ryugafury <pulkitmantri359@gmail.com>
2025-04-27 23:26:07 +0530

    Add files via upload

---
+++ list document questions/q5.py
@@ -0,0 +1,11 @@
+list =[]
+def upper(l):
+    l =["hello","bye","Python","how","hey"]
+    g=[]
+    for i in range(len(l)):
+        f=l[i].upper()
+        g.append(f)
+    print(g)
+       
+upper(list)
+        
