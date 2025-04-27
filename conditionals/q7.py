c36a15b45438119e99477f2f69ea46f897e30f22
ryugafury <pulkitmantri359@gmail.com>
2025-04-27 23:32:13 +0530

    Add files via upload

---
+++ conditionals/q7.py
@@ -0,0 +1,5 @@
+year=int(input("Enter a year"))
+if(year%4==0):
+    print("leap year")
+else:
+    print("Not a leap year")
---
+++ conditionals/q9.py
@@ -0,0 +1,5 @@
+a= int(input("Enter a number"))
+if(a>0):
+    print(f"the absolute value of |a| is {a} ")
+else:
+    print(f"The absolute value of |a| is {-a}")
